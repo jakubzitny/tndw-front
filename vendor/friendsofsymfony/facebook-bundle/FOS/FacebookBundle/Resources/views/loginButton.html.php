030d6289786bb0eaefa0e3cf6e088428a47e2b03
Jakub Žitný <zitnyjak@fit.cvut.cz>
2013-05-13 18:44:46 +0200

    vendors removed from git

--- vendor/friendsofsymfony/facebook-bundle/FOS/FacebookBundle/Resources/views/loginButton.html.php
+++
@@ -1,2 +0,0 @@
-<fb:login-button show-faces="<?php echo $showFaces; ?>" width="<?php echo $width; ?>" max-rows="<?php echo $maxRows; ?>" scope="<?php echo $scope; ?>" registration-url="<?php echo $registrationUrl; ?>" size="<?php echo $size; ?>"  autologoutlink="<?php echo $autologoutlink; ?>"><?php echo $label; ?></fb:login-button>
-
