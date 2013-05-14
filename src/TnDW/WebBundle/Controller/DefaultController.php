e0f34dc0b2d4b10d838848f8e214c7ec76ca5ce4
Jakub Žitný <zitnyjak@fit.cvut.cz>
2013-05-14 10:49:44 +0200

    css fixes across whole site

--- src/TnDW/WebBundle/Controller/DefaultController.php
+++ src/TnDW/WebBundle/Controller/DefaultController.php
@@ -6,7 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
     public function indexAction() {
-		$distros = $this->getDoctrine()->getRepository('TnDWWebBundle:Distro')->findAll();
+		$parameters = $this->container->parameters;
+		$ph = pg_Connect("dbname=".$parameters['database_name_trol']." user=".$parameters['database_user_trol']." password=".$parameters['database_password_trol']);
+		$result = pg_query($ph, "select * from backing_distribution limit 25");
+		$distros = array();
+		while ($distro = pg_fetch_object($result)){
+			$distros[] = $distro;
+		}
+
         if (!$distros) {
             throw $this->createNotFoundException('No distro found..');
         }
@@ -23,9 +30,9 @@ class DefaultController extends Controller {
         
         $topupdates = array_slice($updates, 0, 2);
         $topnews = array_slice($news, 0, 2);
-        $topdistros = array_slice($distros, 0, 25);
+        //$topdistros = array_slice($distros, 0, 25);
         return $this->render('TnDWWebBundle:Default:index.html.twig', array(
-			'distros' => $topdistros,
+			'distros' => $distros,
             'news' => $topnews,
             'updates' => $topupdates,
 		));
